9ea278133ec8a7fbe730e76f0b8db456f88dd59e
finite0104 <how3265@naver.com>
2016-11-20 15:36:13 +0900

    kill execl

---
+++ gcc_kill/kill.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main(int argv, char *argc[])
+{
+	if(argv < 2)
+	{
+		printf("enter the process pid\n");
+		exit(1);
+	}
+
+	pid_t pid;
+
+	pid = fork();
+	if(pid == 0)
+	{
+//		execl("/bin/ls","ls","-l", (char*)0);
+//		execl("/sbin/shutdown","shutdown","-r","1",NULL);
+		execl("/bin/kill", "kill", "-15", argc[1], NULL);
+		perror("execl failed");
+	}
+	else if(pid > 0)
+	{
+		wait((int*)0);
+		printf("process kill completed\n");
+		exit(0);
+	}
+	else
+	{
+		perror("fork failed");
+	}
+}
