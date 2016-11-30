070386a1e513c378a86fe107d18aa23a316c5136
finite0104 <how3265@naver.com>
2016-12-01 00:10:54 +0900

    통합

---
+++ SeokHyun/dialog.cpp
@@ -0,0 +1,27 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include <QtWidgets>
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent,Qt::FramelessWindowHint),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    this->showMaximized();
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    if(ui->lineEdit->text() != NULL)
+        this->close();
+}
+
+QString Dialog::getNumber(){
+    QString temp = ui->lineEdit->text();
+    return temp;
+}
